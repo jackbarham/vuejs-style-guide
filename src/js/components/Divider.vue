a60ec3f69b2fc482041fed75c2f8e061bbff82ae
jackbarham <jack@airboxmedia.com>
2016-01-14 15:38:39 +0000

    hr styling

---
+++ src/js/components/Divider.vue
@@ -0,0 +1,22 @@
+<template>
+    <hr :class="devideType">
+</template>
+
+<script>
+    export default {
+
+        props: ['type'],
+
+        computed: {
+            devideType() {
+                var type = this.type;
+                return {
+                    'divider': true,
+                    'wide': type == 'wide',
+                    'narrow': type == 'narrow'
+                }
+            }
+        }
+
+    };
+</script>
--- src/js/main.js
+++ src/js/main.js
@@ -3,13 +3,9 @@ var Vue = require('vue');
 import Top from './components/Top.vue';
 import Typography from './components/Typography.vue';
 import Button from './components/Button.vue';
+import Divider from './components/Divider.vue';
 
 new Vue({
     el: '#app',
-
-    components: { Top, Typography, Button },
-
-    ready() {
-        // alert('Vue and Vueify all set to go!');
-    }
+    components: { Top, Typography, Button, Divider },
 });
