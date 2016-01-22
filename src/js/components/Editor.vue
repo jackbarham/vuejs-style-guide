49b396d0eb64c067db9fd4ecd4547a0bb7f86b7e
jackbarham <jack@airboxmedia.com>
2016-01-22 17:57:54 +0000

    Editor styling updates

--- src/js/components/Editor.vue
+++ src/js/components/Editor.vue
@@ -1,13 +1,13 @@
 <template>
     <div class="form-item">
-        <label class="form-label" v-if="!focused">Rich text editor</label>
-        <label class="form-label form-help" v-if="focused">Highlight text to edit</label>
+        <div class="cf">
+            <label class="form-label left">Rich text editor</label>
+            <label class="form-label form-help right" v-if="focused">Highlight text to edit</label>
+        </div>
         <div class="editable" @focus="focused = true" @blur="focused = false">
-            <p><b>Nightclub</b> - The Victorian building opened in 1886 and was originally used as a warehouse and stables by the <a href="https://en.wikipedia.org/wiki/Great_Northern_Railway_(Great_Britain)">Great Northern Railway Company</a>.</p>
-            <p>The building later became a warehouse for Booth’s Dry Gin distillery before changing hands in 1985 and becoming a bar.</p>
+            {{{ content }}}
         </div>
     </div>
-    <!--<pre>{{ $data | json }}</pre>-->
 </template>
 
 
@@ -16,7 +16,7 @@
 
         data() {
             return {
-                content: '',
+                content: '<p><b>Nightclub</b> - The Victorian building opened in 1886 and was originally used as a warehouse and stables by the <a href=\"https://en.wikipedia.org/wiki/Great_Northern_Railway_(Great_Britain)\">Great Northern Railway Company</a>.</p><p>The building later became a warehouse for Booth’s Dry Gin distillery before changing hands in 1985 and becoming a bar.</p>',
                 focused: false,
             }
         },
@@ -27,7 +27,48 @@
                 fontawesome: true,
                 imageDragging: false,
                 toolbar: {
-                    buttons: ['bold', 'italic', 'underline', 'anchor'],
+                    buttons: [
+                        {
+                            name: 'bold',
+                            action: 'bold',
+                            aria: 'bold',
+                            tagNames: ['b', 'strong'],
+                            style: {
+                                prop: 'font-weight',
+                                value: ''
+                            },
+                            useQueryState: true,
+                            contentDefault: '<i class="icon-bold"></i>',
+                            contentFA: '<i class="icon-bold"></i>'
+                        },
+                        {
+                            name: 'italic',
+                            action: 'italic',
+                            aria: 'italic',
+                            tagNames: ['i', 'em'],
+                            style: {
+                                prop: 'font-style',
+                                value: ''
+                            },
+                            useQueryState: true,
+                            contentDefault: '<i class="icon-italic"></i>',
+                            contentFA: '<i class="fa fa-italic"></i>'
+                        },
+                        {
+                            name: 'underline',
+                            action: 'underline',
+                            aria: 'underline',
+                            tagNames: ['u'],
+                            style: {
+                                prop: 'text-decoration',
+                                value: ''
+                            },
+                            useQueryState: true,
+                            contentDefault: '<i class="icon-underline"></i>',
+                            contentFA: '<i class="fa fa-underline"></i>'
+                        },
+                        'anchor'
+                    ]
                 },
                 anchor: {
                     placeholderText: 'Type a link',
